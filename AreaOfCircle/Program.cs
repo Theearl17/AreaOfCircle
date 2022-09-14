9302580477c7df9fad3576b3d32b578eda086741
earl starks <earlstarks@rocketmail.com>
2022-09-14 12:29:03 -0500

    AreaOfCircle

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter the radius of your circle?");
+var radius = double.Parse(Console.ReadLine());
+Console.WriteLine($" Your area of a circle is:{AreaOfCircle(radius)}");
+
+
+    static double AreaOfCircle(double radius)
+
+{
+    return Math.PI * Math.Pow (radius, 2); 
+}
